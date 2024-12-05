16c09b17cc97f0b3b57bd2ec1744eec850809b8b
Scamander1137 <scamandrew113@proton.me>
2024-12-05 20:40:26 +0700

    Update

--- ihsButton.js
+++
@@ -1,7 +0,0 @@
-const ihs = document.getElementById("ihs");
-
-ihs.addEventListener('click', () => {
-
-    window.location.href='https://discord.gg/UcpVCEAawM'
-
-})
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const box = document.getElementById('hover-mainDiv');
+const ihs = document.getElementById("ihs");
 let debounce = false;
 
 function wait(ms) {
@@ -25,3 +26,9 @@ box.addEventListener('click', () => {
     window.location.href='https://i.pinimg.com/564x/2e/1d/6b/2e1d6be769bcd31c101cbe88636e019c.jpg';
 
 });
+
+ihs.addEventListener('click', () => {
+
+    window.location.href='https://discord.gg/UcpVCEAawM'
+
+})
